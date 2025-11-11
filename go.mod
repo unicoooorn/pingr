b69a708c68f8802c9da07c0db979e73765ce050d
vch7lv <vch7lv@example.com>
2025-11-11 23:26:02 +0300

    add more checkers and tests

--- go.mod
+++ go.mod
@@ -3,14 +3,19 @@ module github.com/unicoooorn/pingr
 go 1.25.1
 
 require (
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.10 // indirect
+	github.com/go-ping/ping v1.2.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.28.0 // indirect
 	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
+	github.com/go-redis/redis/v8 v8.11.5 // indirect
 	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
@@ -26,7 +31,12 @@ require (
 	github.com/subosito/gotenv v1.6.0 // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
 	golang.org/x/crypto v0.42.0 // indirect
+	golang.org/x/net v0.43.0 // indirect
+	golang.org/x/sync v0.17.0 // indirect
 	golang.org/x/sys v0.38.0 // indirect
 	golang.org/x/text v0.30.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250804133106-a7a43d27e69b // indirect
+	google.golang.org/grpc v1.76.0 // indirect
+	google.golang.org/protobuf v1.36.6 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/checker/checker.go
@@ -0,0 +1,69 @@
+package checker
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/unicoooorn/pingr/internal/config"
+	"github.com/unicoooorn/pingr/internal/model"
+	"github.com/unicoooorn/pingr/internal/service"
+)
+
+func NewChecker(config *config.Config) service.Checker {
+	return &CheckerImpl{Config: config}
+}
+
+var _ service.Checker = &CheckerImpl{}
+
+type CheckerImpl struct {
+	Config *config.Config
+}
+
+func (r *CheckerImpl) Check(ctx context.Context, subsystem string) (model.CheckResult, error) {
+	subsystem_cfg, exist := r.Config.Backends[subsystem]
+	if !exist {
+		return model.CheckResult{Status: model.PingStatusNotOk, Details: "not found"}, fmt.Errorf("not found cfg for '%s'", subsystem)
+	}
+	if subsystem_cfg.Type == "" {
+		return model.CheckResult{Status: model.PingStatusNotOk, Details: "type empty"}, fmt.Errorf("type empty or not found for '%s'", subsystem)
+	}
+
+	timeout := time.Duration(subsystem_cfg.Timeout) * time.Second
+	addr := fmt.Sprintf("%s:%d", subsystem_cfg.Host, subsystem_cfg.Port)
+
+	switch subsystem_cfg.Type {
+	case "http":
+		if subsystem_cfg.URL == "" {
+			return model.CheckResult{}, fmt.Errorf("http checker: missing url")
+		}
+		return CheckHttpHealth(ctx, subsystem_cfg.URL, subsystem_cfg.Headers, timeout)
+	case "grpc":
+		if subsystem_cfg.URL == "" {
+			return model.CheckResult{}, fmt.Errorf("postgres checker: missing url (DSN)")
+		}
+		return CheckGrpcHealth(ctx, addr, timeout)
+	case "icmp":
+		if subsystem_cfg.Host == "" {
+			return model.CheckResult{}, fmt.Errorf("icmp checker: missing host")
+		}
+		return CheckIcmpHealth(ctx, subsystem_cfg.Host, timeout)
+	case "tcp":
+		if subsystem_cfg.Host == "" || subsystem_cfg.Port == 0 {
+			return model.CheckResult{}, fmt.Errorf("tcp checker: missing host and/or port")
+		}
+		return CheckTcpHealth(ctx, subsystem_cfg.Host, subsystem_cfg.Port, timeout)
+	case "redis":
+		if subsystem_cfg.Host == "" || subsystem_cfg.Port == 0 {
+			return model.CheckResult{}, fmt.Errorf("redis checker: missing host and/or port")
+		}
+		return CheckRedisHealth(ctx, addr, timeout)
+	case "postgres":
+		if subsystem_cfg.URL == "" {
+			return model.CheckResult{}, fmt.Errorf("postgres checker: missing url (DSN)")
+		}
+		return CheckPostgresHealth(ctx, subsystem_cfg.URL, timeout)
+	default:
+		return model.CheckResult{}, fmt.Errorf("unknown backend type: '%s'", subsystem_cfg.Type)
+	}
+}
